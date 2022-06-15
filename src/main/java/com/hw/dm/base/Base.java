5c3916c5b07cc052609060e72d85cc1168ebdf63
feiqing <fei88591@163.com>
2022-06-15 14:59:05 +0800

    初始化：hw

---
+++ src/main/java/com/hw/dm/base/Base.java
@@ -0,0 +1,12 @@
+package com.hw.dm.base;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * 日志
+ */
+public class Base {
+    protected Logger logger = LoggerFactory.getLogger(this.getClass());
+
+}
--- src/main/java/com/hw/dm/zero/Demo.java
+++ src/main/java/com/hw/dm/zero/Demo.java
@@ -1,8 +1,14 @@
 package com.hw.dm.zero;
 
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 public class Demo {
 
+    private static Logger logger = LoggerFactory.getLogger(Demo.class);
+
     public static void main(String[] args) {
-        System.out.println(1111);
+        logger.info("init demo ,this is new demo = {}","hw");
     }
 }
