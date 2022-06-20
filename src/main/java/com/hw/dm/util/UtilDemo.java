3cc6183cadd6a12629f980aa70bbbd966527cf82
feiqing <fei88591@163.com>
2022-06-20 16:40:27 +0800

    新增：工具类汇总

---
+++ src/main/java/com/hw/dm/util/UtilDemo.java
@@ -0,0 +1,43 @@
+package com.hw.dm.util;
+
+import org.springframework.util.CollectionUtils;
+
+import java.util.ArrayList;
+import java.util.Collections;
+
+/**
+ * 常用utils
+ */
+public class UtilDemo {
+
+    public static void main(String[] args) {
+        Collections.sort(new ArrayList<String>());
+        CollectionUtils.isEmpty(new ArrayList<>());
+
+
+
+    }
+
+    /**
+     *  Collections
+     *  CollectionUtils
+     *  Lists
+     *  Objects
+     *  BooleanUtils
+     *  StringUtils
+     *  Assert
+     *  IOUtils
+     *  MDC
+     *  ClassUtils
+     *  BeanUtils
+     *  ReflectionUtils
+     *  Base64Utils
+     *  StandardCharsets
+     *  DigestUtils
+     *  SerializationUtils
+     *  HttpStatus
+     *  帖子地址：https://mp.weixin.qq.com/s/GHQDBSxW1MfZHDOvryklmA
+     *
+     */
+
+}
